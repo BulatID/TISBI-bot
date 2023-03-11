4a77ae8a525cfb3f3a4e755bd253593525680cc7
shiryauwu <70473372+shiryauwu@users.noreply.github.com>
2023-03-12 00:08:03 +0300

    Add files via upload

---
+++ TISBI Telegram Bot/FileSenderTelegram/TisbiContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserTISBINew
+{
+    internal class TisbiContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public TisbiContext() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=tisbidb;Trusted_Connection=True;");
+        }
+    }
+}
